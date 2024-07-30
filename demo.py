1ee91f9c30345c0ac824256b5930c44c5db5ed72
abishekpranavsa <abishekpranav2004@gmail.com>
2024-07-30 23:48:55 +0530

    commit 2

--- demo.py
+++ demo.py
@@ -8,12 +8,10 @@ c=7
 d=7
 print("Multiplication of two numbers : ",c*d)
 
-<<<<<<< HEAD
 g=5
 h=5
 print("Subtration of two numbers : ",g-h)
-=======
+
 e=16
 f=4
 print("Division of two numbers : ",e/f)
->>>>>>> 30f3596fe245382dcb81e875c9b68d1b2703b5fb
